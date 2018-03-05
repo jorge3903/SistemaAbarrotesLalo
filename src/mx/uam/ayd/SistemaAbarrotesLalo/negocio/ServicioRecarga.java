cf3fbc60c7f4f2279f8f5ced7e20b41760acde31
nallelyRen <37052280+nallelyRen@users.noreply.github.com>
2018-03-04 18:32:15 -0600

    Update ServicioRecarga.java

--- src/mx/uam/ayd/SistemaAbarrotesLalo/negocio/ServicioRecarga.java
+++ src/mx/uam/ayd/SistemaAbarrotesLalo/negocio/ServicioRecarga.java
@@ -2,20 +2,23 @@
 package mx.uam.ayd.SistemaAbarrotesLalo.negocio;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
+import mx.uam.ayd.SistemaAbarrotesLalo.modelo.Recarga;
 import mx.uam.ayd.SistemaAbarrotesLalo.persistencia.DAORecarga;
 
 /**
  *
- * @author lalo
+ * @author nallely
  */
 public class ServicioRecarga {
 DAORecarga daoRecarga1=new DAORecarga();
 int montoTotal=0, numeroRecargas=0;
+Recarga recarga1;
     
     /**
-     *
+     *  Devuelve el monto total de recargas dentro de un periodo de tiempo
      * @param periodo
-     * @return
+     * @return montoTotal
      * @throws SQLException
      */
     public int dameMontoTotalRecargas(String periodo) throws SQLException {
@@ -24,14 +27,27 @@ int montoTotal=0, numeroRecargas=0;
     }
 
     /**
-     *
+     * Devuelve el numero de recargas realizadas dentro de un periodo de tiempo
      * @param periodo
-     * @return
+     * @return numeroRecargas
      * @throws SQLException
      */
     public int dameNumeroDeRecargas(String periodo) throws SQLException {
        numeroRecargas=daoRecarga1.recuperaTamaño(periodo);
        return numeroRecargas;
+    }
+
+    /**
+     * Agrega una entidad de recarga dentro de la base de datos
+     * @param monto
+     * @param fechaActual
+     * @return
+     * @throws SQLException
+     */
+    public boolean AgregarRecarga(int monto, LocalDate fechaActual) throws SQLException {
+       recarga1=new Recarga(fechaActual, monto); 
+       boolean estado=daoRecarga1.AgregaRecarga(recarga1);
+       return estado;
     }
     
 }
