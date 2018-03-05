05a1dd0532fe174838096cd833aefc0e08bc989b
nallelyRen <37052280+nallelyRen@users.noreply.github.com>
2018-03-04 18:47:17 -0600

    Update Recarga.java

--- src/mx/uam/ayd/SistemaAbarrotesLalo/modelo/Recarga.java
+++ src/mx/uam/ayd/SistemaAbarrotesLalo/modelo/Recarga.java
@@ -1,23 +1,24 @@
 
 package mx.uam.ayd.SistemaAbarrotesLalo.modelo;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Recarga {
-    private Date fecha;
+    private LocalDate fechaActual;
     private int monto;
     
-    public Recarga(Date fecha, int monto) {
-        this.fecha = fecha;
-        this.monto = monto;
+    public Recarga(LocalDate fechaActual, int monto) {
+      this.fechaActual=fechaActual;
+      this.monto=monto;
     }
 
-    public Date getFecha() {
-        return fecha;
+    public LocalDate getFechaActual() {
+        return fechaActual;
     }
 
-    public void setFecha(Date fecha) {
-        this.fecha = fecha;
+    public void setFechaActual(LocalDate fechaActual) {
+        this.fechaActual = fechaActual;
     }
 
     public int getMonto() {
@@ -26,5 +27,7 @@ public class Recarga {
 
     public void setMonto(int monto) {
         this.monto = monto;
-    }    
+    }
+
+    
     } 
